f456a5bbe443de92e31cf1540c1c5d4be08d38ff
StasiaKuzya <kuzina-sta@mail.ru>
2024-05-14 15:14:49 +0300

    Initial Commit

---
+++ PhotoEditor/PhotoEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoEditorApp.swift
+//  PhotoEditor
+//
+//  Created by Анастасия on 14.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct PhotoEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
